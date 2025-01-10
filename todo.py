b17d23420f7115f79e91292bd4528a72aea9dc7b
Anirudh <95073991+ani-2008@users.noreply.github.com>
2025-01-10 20:51:48 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -3,33 +3,33 @@ import json
 with open("list.json") as f:
     data = f.read()
 
-todoDict = json.loads(data)
+todo_dict = json.loads(data)
 
-c = 0
-def check_through_taskes(c,task,label,status):
-    for i in range(c):
-        
-        if task == todoDict[label][str(i)][0] and status != todoDict[label][str(i)][1]:
+count = 0
+def check_through_taskes(count,task,label,status):
+    for i in range(count):
+        if str(i) not in todo_dict[label]:
+            continue
+        elif task == todo_dict[label][str(i)][0] and status != todo_dict[label][str(i)][1]:
             return True
         else:
             return False
 
 def delete_task(choice):
-    t = False
-    s = False
-    mc = False
-    mlbl = False
-    sc = False
+    del_task = False
+    del_status = False
+    
+    string_count = False
     if choice == "y":
         ask_label = input("Label:").strip()
-        if ask_label in todoDict:
+        if ask_label in todo_dict:
             ask_task = input("Task:").strip()
-            for i in todoDict[ask_label].keys():
+            for i in todo_dict[ask_label].keys():
                 
-                if ask_task in todoDict[ask_label][i]:
-                    sc = i
-                    t = todoDict[ask_label][i][0]
-                    s =  todoDict[ask_label][i][1]
+                if ask_task in todo_dict[ask_label][i]:
+                    string_count = i
+                    del_task = todo_dict[ask_label][i][0]
+                    del_status =  todo_dict[ask_label][i][1]
                     
                     break
                 else:
@@ -38,101 +38,112 @@ def delete_task(choice):
         else:
             print("Label not found")
             return False
-    if t and s and sc:
+    if del_task and del_status and string_count:
         
-        del todoDict[ask_label][sc][0]
-        del todoDict[ask_label][sc][0]
-    if mc and sc:
-        del todoDict[ask_label][sc]
-    if mlbl:
-        del todoDict[ask_label]
+        del todo_dict[ask_label][string_count][0]
+        del todo_dict[ask_label][string_count][0]
     
-    if todoDict[ask_label][sc] == []:
-        del todoDict[ask_label][sc]
-    if todoDict[ask_label] == {}:
-        del todoDict[ask_label]
-    return todoDict
+    
+    if todo_dict[ask_label][string_count] == []:
+        del todo_dict[ask_label][string_count]
+    if todo_dict[ask_label] == {}:
+        del todo_dict[ask_label]
+    
+    new_dict = {}
+    list_c = []
+    for str_c in todo_dict[ask_label].keys():
+        list_c.append(int(str_c))
+    
 
+    return todo_dict,string_count
+
+def change_dict_json(obj):
+    return ((((((json.dumps(obj,indent=3)).replace("{","")).replace("}",""))).replace("[","")).replace("]","")).replace(",","")
 
 def main():
-    global todoDict
+    global todo_dict
+    global count
+    global string_count
+    string_count = "0"
     while True:
+        
         print("To exit type exit")
         print("To display type display")
         print("To update your task you need to exit or display")
         choice = input("Do you want to delete any task ? (y/n)").lower()
         if choice == "y":
-            print(((((((json.dumps(delete_task('y'),indent=3)).replace("{","")).replace("}",""))).replace("[","")).replace("]","")).replace(",",""))
-            jsonTodoDict = json.dumps(todoDict)
+            todo_dict,string_count = delete_task('y')
+            
+            print(change_dict_json(todo_dict))
+            jsonTodo_dict = json.dumps(todo_dict)
             
             with open("list.json","w") as f:
-                data = f.write(jsonTodoDict)
+                data = f.write(jsonTodo_dict)
             with open("list.json") as f:
                 data = f.read()
-            todoDict = json.loads(data)
+            todo_dict = json.loads(data)
 
         elif choice == "n":
             choice = input("Do you want to add any task ?\nIf no then you can see your current tasks (y/n)").lower()
             if choice == "y":
-                
-                print("To exit type exit")
-                print("To display type display")
                 label = input("Label: ").strip()
                 if label.lower() == "exit":
                     break
                 elif label.lower() == "display":
-                    print(((((((json.dumps(todoDict,indent=3)).replace("{","")).replace("}",""))).replace("[","")).replace("]","")).replace(",",""))
+                    print(change_dict_json(todo_dict))
                     continue
                 task = input("Task: ").strip()
                 if task.lower() == "exit":
                     break
                 elif task.lower() == "display":
-                    print(((((((json.dumps(todoDict,indent=3)).replace("{","")).replace("}",""))).replace("[","")).replace("]","")).replace(",",""))
+                    print(change_dict_json(todo_dict))
                     continue
                 status = input("Status: ").strip()
                 if status.lower() == "exit":
                     break
                 elif status.lower() == "display":
-                    print(((((((json.dumps(todoDict,indent=3)).replace("{","")).replace("}",""))).replace("[","")).replace("]","")).replace(",",""))
+                    print(change_dict_json(todo_dict))
                     continue
-                if label not in todoDict:
-                    c = 0
-                    todoDict.update({label:{str(c):[task,status]}})
-                    c+=1
+                if label not in todo_dict:
+                    count = 0
+                    todo_dict.update({label:{str(count):[task,status]}})
+                    count +=1
                 
                 else:
-                    
-                    if c != 0:
+                    count = int(string_count)
+                    if count != 0:
                         
-                        if check_through_taskes(c,task,label,status):
-                            todoDict[label].update({str(c-1):[task,status]})
+                        if check_through_taskes(count,task,label,status):
+                            todo_dict[label].update({str(count-1):[task,status]})
                             
                         else:
-                            todoDict[label].update({str(c):[task,status]})
-                            c+=1
+                            todo_dict[label].update({str(count):[task,status]})
+                            count+=1
                             
                     else:
-                        if task in todoDict[label][str(c)]:
-                            todoDict[label].update({str(c):[task,status]})
-                            c+=1
+                        if str(count) not in todo_dict[label]:
+                            todo_dict[label].update({str(count):[task,status]})
+                        elif task in todo_dict[label][str(count)]:
+                            todo_dict[label].update({str(count):[task,status]})
+                            count+=1
                         else:
-                            c+=1
-                            todoDict[label].update({str(c):[task,status]})
+                            count+=1
+                            todo_dict[label].update({str(count):[task,status]})
             elif choice == "n":
-                print(((((((json.dumps(todoDict,indent=3)).replace("{","")).replace("}",""))).replace("[","")).replace("]","")).replace(",",""))
+                print(change_dict_json(todo_dict))
                 continue
             elif choice == "exit":
                 break
             elif choice == "display":
-                print(((((((json.dumps(todoDict,indent=3)).replace("{","")).replace("}",""))).replace("[","")).replace("]","")).replace(",",""))
+                print(change_dict_json(todo_dict))
                 continue
         elif choice == "exit":
             break
         elif choice == "display":
-            print(((((((json.dumps(todoDict,indent=3)).replace("{","")).replace("}",""))).replace("[","")).replace("]","")).replace(",",""))
+            print(change_dict_json(todo_dict))
             break        
 
 main()
-jsonTodoDict = json.dumps(todoDict)
+jsonTodo_dict = json.dumps(todo_dict)
 with open("list.json","w") as f:
-    data = f.write(jsonTodoDict)
+    data = f.write(jsonTodo_dict)
